682ed1b4596ceda7467fb9cc01738389dacd1471
moisesaguirre <moisesaguirre22@gmail.com>
2026-02-20 20:31:37 -0300

    perfil condicional

--- src/js/app.js
+++ src/js/app.js
@@ -22,25 +22,29 @@ import "../style/index.css";
         city: null
     }
  */
+
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  if (variables.name == null) variables.name = "Lucy";
+  if (variables.lastName == null) variables.lastName = "Boilett";
+  if (variables.role == null) variables.role = "Developer";
+  if (variables.city == null) variables.city = "City";
+  if (variables.country == null) variables.country = "Country";
   if (variables.socialMediaPosition != "position-left")
     variables.socialMediaPosition = "position-right";
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name == null ? "Name" : variables.name} ${
-    variables.lastName == null ? "LastName" : variables.lastName
-  }
-  </h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name} ${variables.lastName}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}</h3>
+          <h2>${variables.country}</h2>
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
